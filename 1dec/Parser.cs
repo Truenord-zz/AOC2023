7fbf048fd6b9ef84c0d5d77e3c6eac788afabdd2
Truenord-zz <andreascarlson19@gmail.com>
2023-12-02 22:15:44 +0100

    made a file for the tasks input, and setup the skeleton of a parser

---
+++ 1dec/Parser.cs
@@ -0,0 +1,10 @@
+namespace parser
+{
+    public class Parser
+    {
+        public Parser()
+        {
+            // ...
+        }
+    }
+}
---
+++ 1dec/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using parser;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var parser = new Parser();
+    }
+}
